17cd81856c49c7b328737063100c0b974406f145
Daniel Mattsson <danielm@wip.se>
2020-02-10 13:51:14 +0100

    Added eslin rules

--- generator.js
+++ generator.js
@@ -3,7 +3,9 @@ module.exports = (api, options, rootOptions) => {
       dependencies: {},
       eslintConfig: {
         rules: {
-          "vue/require-v-for-key": 0
+          "vue/require-v-for-key": 'off',
+          'no-unused-vars': api.makeJSOnlyValue(`process.env.NODE_ENV === 'development' ? 'off' : 'warn'`),
+          'vue/no-unused-components': api.makeJSOnlyValue(`process.env.NODE_ENV === 'development' ? 'off' : 'warn'`),
         }
       }
     });
